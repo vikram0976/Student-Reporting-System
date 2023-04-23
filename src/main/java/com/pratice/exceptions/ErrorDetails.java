bb620d75882aae49744dcac75bf5966710bdb343
Vikram Chaurasia <chaurasiavikram23@gmail.com>
2023-04-23 19:30:57 +0530

    exception package

---
+++ src/main/java/com/pratice/exceptions/ErrorDetails.java
@@ -0,0 +1,22 @@
+package com.pratice.exceptions;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ErrorDetails {
+	
+	private LocalDateTime timeStamp;
+	private String message;
+	private String details;
+
+}
---
+++ src/main/java/com/pratice/exceptions/StudentException.java
@@ -0,0 +1,16 @@
+package com.pratice.exceptions;
+
+public class StudentException extends Exception{
+
+	public StudentException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
